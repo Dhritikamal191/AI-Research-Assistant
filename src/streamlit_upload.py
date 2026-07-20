66d3d1454e0fc9081d1e18ec32b43024ee30897d
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-07-20 12:00:27 +0530

    Create streamlit_upload.py

---
+++ src/streamlit_upload.py
@@ -0,0 +1,13 @@
+import os
+
+UPLOAD_DIR = "data/uploads"
+
+def save_uploaded_file(uploaded_file):
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+    save_path = os.path.join(UPLOAD_DIR, uploaded_file.name)
+
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    return save_path
