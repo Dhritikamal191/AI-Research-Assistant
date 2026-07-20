73de85ecbc16d6b33c2f45326e2b639f9051f2dd
Dhriti kamal Das <dkamaldas2002@gmail.com>
2026-07-20 12:01:30 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,34 +5,13 @@ Utility Functions
 """
 
 import os
-import shutil
 
 UPLOAD_DIR = "data/uploads"
 
-
 def ensure_directories():
-
     os.makedirs(UPLOAD_DIR, exist_ok=True)
-
     os.makedirs("vectorstore", exist_ok=True)
-
     os.makedirs("logs", exist_ok=True)
 
-
-async def save_uploaded_file(uploaded_file):
-
-          ensure_directories()
-
-          save_path = os.path.join(UPLOAD_DIR,uploaded_file.filename)
-
-          with open(save_path, "wb") as f:
-
-               contents = await uploaded_file.read()
-               f.write(contents)
-
-          return save_path
-
-
 def allowed_file(filename):
-
     return filename.lower().endswith(".pdf")
